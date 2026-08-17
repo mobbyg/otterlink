72ae0019141dc02c1a1e64cb9c5672d8f6cd2912
Rich L <mobbyg@gmail.com>
2026-08-17 17:15:19 -0400

    Implement OSCAR buddy rights reply

--- server/internal/oscar/userinfo.go
+++ server/internal/oscar/userinfo.go
@@ -12,25 +12,26 @@ const (
 	SNACUserInfoServerReady uint16 = 0x0003
 )
 
+// writeUserInfoReady returns the BUDDY rights reply. Family 0x0003 is the
+// OSCAR Buddy service; subtype 0x0002 is the client's rights query and 0x0003
+// is the server's rights reply.
 func (s *Server) writeUserInfoReady(conn net.Conn, sequence uint16, requestID uint32) error {
-	// OSCAR service-ready payload: family version, tool ID, tool version,
-	// distribution number, language, country.
-	payload := make([]byte, 10)
-	binary.BigEndian.PutUint16(payload[0:2], SNACUserInfoFamily)
-	binary.BigEndian.PutUint16(payload[2:4], 1)
-	binary.BigEndian.PutUint16(payload[4:6], 0x0110)
-	binary.BigEndian.PutUint16(payload[6:8], 0)
-	payload[8] = 0
-	payload[9] = 0
+	maxBuddies := make([]byte, 2); binary.BigEndian.PutUint16(maxBuddies, 200)
+	maxWatchers := make([]byte, 2); binary.BigEndian.PutUint16(maxWatchers, 100)
+	maxBroadcasts := make([]byte, 2); binary.BigEndian.PutUint16(maxBroadcasts, 10)
+	maxTemporary := make([]byte, 2); binary.BigEndian.PutUint16(maxTemporary, 50)
 
-	response := SNAC{
-		Family:    SNACUserInfoFamily,
-		Subtype:   SNACUserInfoServerReady,
-		RequestID: requestID,
-		Payload:   payload,
-	}
+	payload, err := EncodeTLVs([]TLV{
+		{Tag: 0x0001, Value: maxBuddies},
+		{Tag: 0x0002, Value: maxWatchers},
+		{Tag: 0x0003, Value: maxBroadcasts},
+		{Tag: 0x0004, Value: maxTemporary},
+	})
+	if err != nil { return errors.New("encode OSCAR buddy rights: " + err.Error()) }
+
+	response := SNAC{Family: SNACUserInfoFamily, Subtype: SNACUserInfoServerReady, RequestID: requestID, Payload: payload}
 	if err := writeFrame(conn, Frame{Channel: ChannelData, Sequence: sequence, Payload: response.Encode()}); err != nil {
-		return errors.New("write OSCAR user-info ready: " + err.Error())
+		return errors.New("write OSCAR buddy rights reply: " + err.Error())
 	}
 	return nil
 }
