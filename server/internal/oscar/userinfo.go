3e6c34b748dd6129d1d96b68f9b8024a4cf85d85
Rich L <mobbyg@gmail.com>
2026-08-17 16:07:56 -0400

    Add OSCAR user info service negotiation

---
+++ server/internal/oscar/userinfo.go
@@ -0,0 +1,36 @@
+package oscar
+
+import (
+	"encoding/binary"
+	"errors"
+	"net"
+)
+
+const (
+	SNACUserInfoFamily       uint16 = 0x0003
+	SNACUserInfoClientReady  uint16 = 0x0002
+	SNACUserInfoServerReady  uint16 = 0x0003
+)
+
+func (s *Server) writeUserInfoReady(conn net.Conn, sequence uint16, requestID uint32) error {
+	// OSCAR service-ready payload: family version, tool ID, tool version,
+	// distribution number, language, country.
+	payload := make([]byte, 10)
+	binary.BigEndian.PutUint16(payload[0:2], SNACUserInfoFamily)
+	binary.BigEndian.PutUint16(payload[2:4], 1)
+	binary.BigEndian.PutUint16(payload[4:6], 0x0110)
+	binary.BigEndian.PutUint16(payload[6:8], 0)
+	binary.BigEndian.PutUint8(payload[8:9], 0)
+	binary.BigEndian.PutUint8(payload[9:10], 0)
+
+	response := SNAC{
+		Family:    SNACUserInfoFamily,
+		Subtype:   SNACUserInfoServerReady,
+		RequestID: requestID,
+		Payload:   payload,
+	}
+	if err := writeFrame(conn, Frame{Channel: ChannelData, Sequence: sequence, Payload: response.Encode()}); err != nil {
+		return errors.New("write OSCAR user-info ready: " + err.Error())
+	}
+	return nil
+}
