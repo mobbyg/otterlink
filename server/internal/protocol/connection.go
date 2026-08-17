ce38a75db3198cbf52327ca6fca6e3816491639e
Rich L <mobbyg@gmail.com>
2026-08-17 13:33:37 -0400

    Add line-oriented protocol connection

---
+++ server/internal/protocol/connection.go
@@ -0,0 +1,57 @@
+package protocol
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+)
+
+// Connection implements the development line-oriented protocol. Each message
+// is one JSON object terminated by a newline. The logical envelope is shared
+// with future transports, so clients do not depend on TCP framing details.
+type Connection struct {
+	conn net.Conn
+	read *bufio.Reader
+	enc  *json.Encoder
+}
+
+func NewConnection(conn net.Conn) *Connection {
+	return &Connection{conn: conn, read: bufio.NewReader(conn), enc: json.NewEncoder(conn)}
+}
+
+func (c *Connection) ReadMessage() (Message, error) {
+	var msg Message
+	line, err := c.read.ReadBytes('\n')
+	if err != nil {
+		return msg, err
+	}
+	if len(line) > 64*1024 {
+		return msg, fmt.Errorf("protocol message exceeds 64 KiB")
+	}
+	if err := json.Unmarshal(line, &msg); err != nil {
+		return msg, fmt.Errorf("invalid protocol message: %w", err)
+	}
+	return msg, nil
+}
+
+func (c *Connection) WriteMessage(msg Message) error {
+	return c.enc.Encode(msg)
+}
+
+func (c *Connection) Close() error { return c.conn.Close() }
+
+func ErrorResponse(id uint64, code, message string) Message {
+	ok := false
+	return Message{ID: id, Type: TypeResponse, OK: &ok, Error: &Error{Code: code, Message: message}}
+}
+
+func SuccessResponse(id uint64, service, action string, payload interface{}) Message {
+	ok := true
+	return Message{ID: id, Type: TypeResponse, Service: service, Action: action, OK: &ok, Payload: payload}
+}
+
+func IsClosed(err error) bool {
+	return err == io.EOF || err == net.ErrClosed
+}
