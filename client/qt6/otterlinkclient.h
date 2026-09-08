0614363992497e17df4c62394c36166ae991f7ee
Rich L <mobbyg@gmail.com>
2026-09-08 15:26:52 -0400

    Add Qt buddy management client API

--- client/qt6/otterlinkclient.h
+++ client/qt6/otterlinkclient.h
@@ -19,12 +19,16 @@ public:
     void login(const QString &username, const QString &password);
     void loadDashboard();
     void sendChatMessage(const QString &message);
+    void addBuddy(const QString &username);
+    void removeBuddy(const QString &username);
     void logout();
 
 signals:
     void loggedIn(const QString &displayName);
     void dashboardLoaded(const QStringList &buddies, const QStringList &onlineUsers,
                          const QStringList &chatMessages);
+    void buddyChanged();
+    void chatMessageSent();
     void loggedOut();
     void errorOccurred(const QString &message);
 
