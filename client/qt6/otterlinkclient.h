de4ab91a7f673ff8a08c168823a9ab71a65c0197
Rich L <mobbyg@gmail.com>
2026-09-08 14:38:03 -0400

    Add Qt 6 client service interface

---
+++ client/qt6/otterlinkclient.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QString>
+
+class OtterLinkClient final : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit OtterLinkClient(QObject *parent = nullptr);
+
+    void setBaseUrl(const QString &url);
+    QString baseUrl() const;
+
+    void login(const QString &username, const QString &password);
+    void loadDashboard();
+    void logout();
+
+signals:
+    void loggedIn(const QString &displayName);
+    void dashboardLoaded(const QStringList &buddies, const QStringList &onlineUsers,
+                         const QStringList &chatMessages);
+    void loggedOut();
+    void errorOccurred(const QString &message);
+
+private:
+    QNetworkRequest request(const QString &path) const;
+    void handleReply(QNetworkReply *reply);
+
+    QNetworkAccessManager m_network;
+    QString m_baseUrl = QStringLiteral("http://127.0.0.1:9090");
+    QString m_token;
+};
