dafa8471124193ca4ebf87b66a384d1da087bfcf
Rich L <mobbyg@gmail.com>
2026-09-13 18:54:22 -0400

    Add authenticated identity display helper

---
+++ client/qt6/otteridentitydisplay.cpp
@@ -0,0 +1,46 @@
+#include "otteridentitydisplay.h"
+
+#include "mainwindow.h"
+#include "otterlinkclient.h"
+
+#include <QLabel>
+
+namespace {
+
+void updateIdentityLabel(MainWindow &window, const QString &accountName)
+{
+    auto *label = window.findChild<QLabel *>(QStringLiteral("identityLabel"));
+    if (!label)
+        return;
+
+    const QString username = accountName.trimmed();
+    label->setText(username.isEmpty()
+                       ? QStringLiteral("Connected")
+                       : QStringLiteral("Connected as <b>%1</b>")
+                             .arg(username.toHtmlEscaped()));
+    label->setVisible(true);
+    label->adjustSize();
+}
+
+} // namespace
+
+namespace OtterIdentityDisplay {
+
+void install(MainWindow &window)
+{
+    auto *client = window.findChild<OtterLinkClient *>();
+    if (!client)
+        return;
+
+    QObject::connect(client, &OtterLinkClient::loggedIn, &window,
+                     [&window](const QString &displayName) {
+                         updateIdentityLabel(window, displayName);
+                     });
+
+    QObject::connect(client, &OtterLinkClient::dashboardLoaded, &window,
+                     [&window, client](const QStringList &, const QStringList &, const QStringList &) {
+                         updateIdentityLabel(window, client->accountName());
+                     });
+}
+
+} // namespace OtterIdentityDisplay
