a6161bd20253ad11138f9ed0e81763189784211b
Rich L <mobbyg@gmail.com>
2026-09-13 19:20:58 -0400

    Remove identity display installation

--- client/qt6/main.cpp
+++ client/qt6/main.cpp
@@ -1,7 +1,6 @@
 #include <QApplication>
 
 #include "mainwindow.h"
-#include "otteridentitydisplay.h"
 #include "otterlinkstyle.h"
 
 int main(int argc, char *argv[])
@@ -12,7 +11,6 @@ int main(int argc, char *argv[])
     OtterLinkStyle::install(app);
 
     MainWindow window;
-    OtterIdentityDisplay::install(window);
     window.show();
     return app.exec();
 }
