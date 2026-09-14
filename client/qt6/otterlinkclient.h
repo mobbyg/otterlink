b0ad0cd7b87871c3bcd9bc0f5c56169585887cfa
Rich L <mobbyg@gmail.com>
2026-09-14 11:05:25 -0400

    Add channel-aware chat client API

--- client/qt6/otterlinkclient.h
+++ client/qt6/otterlinkclient.h
@@ -20,6 +20,13 @@ public:
     void login(const QString &username, const QString &password);
     void loadDashboard();
     void sendChatMessage(const QString &message);
+    void sendChatMessage(qint64 channelId, const QString &message);
+    void loadChatChannels();
+    void createChatChannel(const QString &name, bool allowOpsToCreateOps);
+    void joinChatChannel(qint64 channelId);
+    void leaveChatChannel(qint64 channelId);
+    void setChatRole(qint64 channelId, const QString &username, const QString &role);
+    void moderateChatUser(qint64 channelId, const QString &username, const QString &action);
     void addBuddy(const QString &username);
     void removeBuddy(const QString &username);
     void logout();
@@ -31,6 +38,10 @@ signals:
     void buddyAdded(const QString &username);
     void buddyChanged();
     void chatMessageSent();
+    void chatChannelsLoaded(const QJsonArray &channels);
+    void chatChannelLoaded(const QJsonObject &channel, const QJsonArray &members,
+                           const QJsonArray &messages, const QString &role);
+    void chatActionCompleted();
     void loggedOut();
     void errorOccurred(const QString &message);
 
