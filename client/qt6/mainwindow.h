613388a6df0f9a34cfa4cbc1d7a7609346588022
Rich L <mobbyg@gmail.com>
2026-09-13 21:26:58 -0400

    Make service resize capability explicit

--- client/qt6/mainwindow.h
+++ client/qt6/mainwindow.h
@@ -44,7 +44,8 @@ private:
     void setLoggedIn(bool loggedIn);
     void beginConnectionPresentation();
     void rebuildBuddyTree(const QStringList &buddies, const QStringList &onlineUsers);
-    void openServiceWindow(const QString &service, const QString &title, QWidget *content);
+    void openServiceWindow(const QString &service, const QString &title, QWidget *content,
+                           bool resizable = false);
     void closeServiceWindow(OtterServiceWindow *window);
     void closeAllServiceWindows();
     void restoreServicePage(QWidget *page);
