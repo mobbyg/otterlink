d57997e3eaefdc475ea381dfe7849eb0499be0d7
Rich L <mobbyg@gmail.com>
2026-09-14 11:06:41 -0400

    Give Chat its dedicated service widget

--- client/qt6/mainwindow.h
+++ client/qt6/mainwindow.h
@@ -6,6 +6,7 @@
 
 class QFrame;
 class QTreeWidget;
+class OtterChatWidget;
 class OtterHomePage;
 class OtterServiceWindow;
 
@@ -55,6 +56,7 @@ private:
     QTreeWidget *m_buddyTree = nullptr;
     QFrame *m_desktop = nullptr;
     OtterHomePage *m_homePage = nullptr;
+    OtterChatWidget *m_chatWidget = nullptr;
     QHash<QString, QString> m_buddyGroups;
     QHash<QString, QString> m_pendingBuddyGroups;
     QHash<QString, OtterServiceWindow *> m_serviceWindows;
