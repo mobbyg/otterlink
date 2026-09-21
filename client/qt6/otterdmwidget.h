936637e692b1705078538dca43637399a82f2cbb
Rich L <mobbyg@gmail.com>
2026-09-21 07:31:04 -0400

    feat: add private message widget

---
+++ client/qt6/otterdmwidget.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QWidget>
+
+class QListWidget;
+class QLineEdit;
+class OtterLinkClient;
+
+class OtterDmWidget final : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit OtterDmWidget(OtterLinkClient *client, const QString &username,
+                           QWidget *parent = nullptr);
+
+private slots:
+    void loadConversation();
+    void sendMessage();
+    void conversationLoaded(const QJsonObject &conversation);
+    void messageSent(const QJsonObject &message);
+
+private:
+    OtterLinkClient *m_client = nullptr;
+    QString m_username;
+    QListWidget *m_messages = nullptr;
+    QLineEdit *m_input = nullptr;
+};
