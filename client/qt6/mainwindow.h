951336b6da62c9e99eb8b944cd89c136d273fbb6
Rich L <mobbyg@gmail.com>
2026-09-21 07:31:17 -0400

    feat: connect dedicated People widget

--- client/qt6/mainwindow.h
+++ client/qt6/mainwindow.h
@@ -7,6 +7,7 @@
 class QFrame;
 class QTreeWidget;
 class OtterHomePage;
+class OtterPeopleWidget;
 class OtterServiceWindow;
 
 namespace Ui {
@@ -39,6 +40,8 @@ private slots:
     void showError(const QString &message);
     void buddySelectionChanged();
     void buddyAdded(const QString &username);
+    void directUnreadLoaded(const QJsonArray &messages);
+    void openPrivateMessage(const QString &username);
 
 private:
     void setLoggedIn(bool loggedIn);
@@ -55,6 +58,7 @@ private:
     QTreeWidget *m_buddyTree = nullptr;
     QFrame *m_desktop = nullptr;
     OtterHomePage *m_homePage = nullptr;
+    OtterPeopleWidget *m_peopleWidget = nullptr;
     QHash<QString, QString> m_buddyGroups;
     QHash<QString, QString> m_pendingBuddyGroups;
     QHash<QString, OtterServiceWindow *> m_serviceWindows;
