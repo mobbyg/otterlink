45585ed09b3c9202a8e711686325729a814b6221
Rich L <mobbyg@gmail.com>
2026-09-21 07:30:47 -0400

    feat: add dedicated People widget

---
+++ client/qt6/otterpeoplewidget.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <QHash>
+#include <QWidget>
+
+class QMenu;
+class QTreeWidget;
+class QTreeWidgetItem;
+class OtterLinkClient;
+
+class OtterPeopleWidget final : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit OtterPeopleWidget(OtterLinkClient *client, QWidget *parent = nullptr);
+    void setBuddies(const QStringList &buddies, const QStringList &onlineUsers);
+    void setUnread(const QJsonArray &messages);
+    bool away() const { return m_away; }
+
+signals:
+    void privateMessageRequested(const QString &username);
+    void awayRequested(bool away);
+
+private slots:
+    void addBuddy();
+    void removeBuddy();
+    void privateMessage();
+    void toggleAway();
+    void buddySelectionChanged();
+
+private:
+    QString selectedUsername() const;
+    void rebuild();
+    void showContextMenu(const QPoint &pos);
+
+    OtterLinkClient *m_client = nullptr;
+    QTreeWidget *m_tree = nullptr;
+    QHash<QString, QString> m_groups;
+    QHash<QString, int> m_unread;
+    QStringList m_buddies;
+    QStringList m_online;
+    bool m_away = false;
+};
