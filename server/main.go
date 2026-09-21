c738d99b4da3b74c24dd553a8f721ea84af67c6a
Rich L <mobbyg@gmail.com>
2026-09-21 07:29:52 -0400

    feat: wire direct messaging service

--- server/main.go
+++ server/main.go
@@ -78,8 +78,9 @@ func main() {
 	buddyService := buddies.Service{DB: database}
 	presenceService := presence.NewService()
 	chatHub := chat.NewHub(database, 100)
+	dmService := dm.Service{DB: database, Limit: 100}
 	authAPI := api.AuthAPI{Accounts: accountService, Presence: presenceService, Chat: chatHub}
-	webServer := &web.Server{Accounts: accountService, Buddies: buddyService, Presence: presenceService, Chat: chatHub}
+	webServer := &web.Server{Accounts: accountService, Buddies: buddyService, Presence: presenceService, Chat: chatHub, DM: dmService}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /api/health", healthHandler)
